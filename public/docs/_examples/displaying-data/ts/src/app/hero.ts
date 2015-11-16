299acda18e5c4a485eb0afaac8c01d5aced6765f
tamura shingo <tamura.shingo@gmail.com>
2015-11-16 16:16:39 +0000

    2015.11.17
    fix typo
    translate contribute.jade
    translate about
    translate docs
    translate ts

--- public/docs/_examples/displaying-data/ts/src/app/hero.ts
+++
@@ -1,9 +0,0 @@
-// #docregion
-export class Hero {
-  constructor(
-    // #docregion id-parameter
-    public id:number,
-    // #enddocregion id-parameter
-    public name:string) { }
-}
-// #enddocregion
